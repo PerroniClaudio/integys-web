a48fe5297555a9a74375a2116458f9657c5f8ca1
Claudio Perroni <c.perroni@ifortech.com>
2025-01-10 15:53:26 +0100

    rimozione metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,11 +4,11 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Custom404 from "@/components/404";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Page not found",
-};
+// export const metadata: Metadata = {
+//   title: "Page not found",
+// };
 export const dynamic = "force-dynamic";
 export default function NotFoundPage() {
   return (
